ded52a12c8fdac2016c1a64ed4fa286ccf1e28d1
angg <angelagilhotra@gmail.com>
2018-07-21 00:24:08 +0530

    initial commit

---
+++ config/version.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'name'      =>  'Akaunting',
+
+    'code'      =>  'Recurring',
+
+    'major'     =>  '1',
+
+    'minor'     =>  '2',
+
+    'patch'     =>  '11',
+
+    'build'     =>  '',
+
+    'status'    =>  'Stable',
+
+    'date'      =>  '11-July-2018',
+
+    'time'      =>  '16:30',
+
+    'zone'      =>  'GMT +3',
+
+];
